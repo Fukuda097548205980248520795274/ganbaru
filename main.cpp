6321db30d55adfeae3dabc26135d606b785977cd
k024g0083 <k024g0083@g.neec.ac.jp>
2024-12-11 16:13:31 +0900

    num3を追加

--- main.cpp
+++ main.cpp
@@ -8,8 +8,12 @@ int main()
 	int num2 = 40;
 	int* p_num2 = &num2;
 
+	int num3 = 60;
+	int* p_num3 = &num3;
+
 	printf("%d \n" , *p_num1);
 	printf("%d \n" , *p_num2);
+	printf("%d \n" , *p_num3);
 
 	return 0;
 }
