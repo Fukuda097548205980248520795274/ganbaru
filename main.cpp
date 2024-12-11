95c0aa3a98985c0fb7823d8f3008cbfc0a000cae
k024g0083 <k024g0083@g.neec.ac.jp>
2024-12-11 16:12:04 +0900

    num => num1

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 
 int main()
 {
-	int num = 35;
-	int* p_num = &num;
+	int num1 = 35;
+	int* p_num1 = &num1;
 
 	int num2 = 40;
 	int* p_num2 = &num2;
 
-	printf("%d \n" , *p_num);
+	printf("%d \n" , *p_num1);
 	printf("%d \n" , *p_num2);
 
 	return 0;
